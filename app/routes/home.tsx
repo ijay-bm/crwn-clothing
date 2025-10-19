5c38e10c925257e2977ef74bab2465d3f9d48788
ijay <ijay@chalkboard.com.ph>
2025-10-20 04:35:22 +0800

    fix: update home page title to 'Home'

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -8,7 +8,7 @@ import type { Route } from "./+types/home";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "Big React Project" },
+    { title: "Home" },
     { name: "description", content: "YEET!" },
   ];
 }
